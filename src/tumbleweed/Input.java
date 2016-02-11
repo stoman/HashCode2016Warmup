7e290e99b19ca78210a170570627a69185b84012
Stefan Toman <git@stoman.de>
2016-02-11 16:54:59 +0000

    read all inputs

--- src/tumbleweed/Input.java
+++ src/tumbleweed/Input.java
@@ -9,4 +9,38 @@ public class Input {
 	public Input(Scanner s) {
 		//TODO: read input
 	}
+	
+	static LinkedList<Input> readAll() {
+		LinkedList<Path> files = new LinkedList<>();
+		Path path = FileSystems.getDefault().getPath("data");
+		try {
+			listFiles(path, files);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		LinkedList<Input> inputs = new LinkedList<>();
+		for (Path p : files) {
+			try {
+				inputs.add(new Input(new Scanner(p)));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return inputs;
+	}
+
+	static void listFiles(Path path, LinkedList<Path> files) throws IOException {
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+			for (Path entry : stream) {
+				if (Files.isDirectory(entry)) {
+					listFiles(entry, files);
+				} else {
+					if (entry.endsWith(".in")) {
+						files.add(entry);
+					}
+				}
+			}
+		}
+	}
 }
